f6ef9756fb3201adc10e2fe137d012ff57c18a25
Jalindar1 <jalindarpandharkar@gmail.com>
2019-01-14 20:40:50 +0530

    Test Automation using .Net core

---
+++ NetCore/TestAutomation/TestAutomation/Features/SpecFlowFeature1.feature
@@ -0,0 +1,32 @@
+﻿Feature: Add two numbers
+		two numbers should be added.
+		If number not given should be considered as 0
+
+
+@Empty-Input
+Scenario Outline: Add empty numbers
+Given first number <firstNumber>
+And second number <secondNumber>
+When clicked Add
+Then Output should be <result>
+Examples: 
+| firstNumber | secondNumber | result |
+|     0       |      0       | 0      |
+|             |       0      | 0      |
+|             |              | 0      |
+|       100   |              | 100    |
+|             |       50     | 50     |
+|       0     |              | 0      |
+
+@Negative-Input
+Scenario Outline: Add Negative numbers
+Given first number <firstNumber>
+And second number <secondNumber>
+When clicked Add
+Then Output should be <result>
+Examples: 
+| firstNumber | secondNumber | result |
+|      -100   |   -10        | -110   |
+|       100   |   -10        |  90    |
+|      -100   |   10         | -90    |
+|       100   |    -200      | -100   |
--- Test/AutomationTest/AutomationTest/AddSpecFlowFeatureSteps_Manual.cs
+++ Test/AutomationTest/AutomationTest/AddSpecFlowFeatureSteps_Manual.cs
@@ -8,62 +8,62 @@ using Protractor;
 
 namespace AutomationTest
 {
-    [Binding]
-    public class AddSpecFlowFeatureSteps : IDisposable
-    {
-        IWebDriver _driver;
-        NgWebDriver _ngDriver;
+    //[Binding]
+    //public class AddSpecFlowFeatureSteps : IDisposable
+    //{
+    //    IWebDriver _driver;
+    //    NgWebDriver _ngDriver;
 
-        public AddSpecFlowFeatureSteps()
-        {
-            _driver = new ChromeDriver(Directory.GetCurrentDirectory());
-            _ngDriver = new NgWebDriver(_driver);
-            _ngDriver.Url = "http://localhost:4200/calculator/";
-            _ngDriver.Manage().Window.Maximize();
-            _ngDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(60);
-        }
+    //    public AddSpecFlowFeatureSteps()
+    //    {
+    //        _driver = new ChromeDriver(Directory.GetCurrentDirectory());
+    //        _ngDriver = new NgWebDriver(_driver);
+    //        _ngDriver.Url = "http://localhost:4200/calculator/";
+    //        _ngDriver.Manage().Window.Maximize();
+    //        _ngDriver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(60);
+    //    }
 
-      [Given(@"first number (.*)"), Scope(Tag = "Empty-Input")]
-        public void GivenFirstNumber(string firstNumber)
-        {
-            _ngDriver.FindElement(By.Id("Num1")).SendKeys(firstNumber);
-        }
+    //  [Given(@"first number (.*)"), Scope(Tag = "Empty-Input")]
+    //    public void GivenFirstNumber(string firstNumber)
+    //    {
+    //        _ngDriver.FindElement(By.Id("Num1")).SendKeys(firstNumber);
+    //    }
 
-        [Given(@"first number (.*)"), Scope(Tag = "Negative-Input")]
-        public void GivenNumber1(string firstNumber)
-        {
-            _ngDriver.FindElement(By.Id("Num1")).SendKeys(firstNumber);
-        }
+    //    [Given(@"first number (.*)"), Scope(Tag = "Negative-Input")]
+    //    public void GivenNumber1(string firstNumber)
+    //    {
+    //        _ngDriver.FindElement(By.Id("Num1")).SendKeys(firstNumber);
+    //    }
 
-        [Given(@"second number (.*)"), Scope(Tag = "Empty-Input")]
-        public void GivenSecondNumber(string secondNumber)
-        {
-            _ngDriver.FindElement(By.Id("Num2")).SendKeys(secondNumber);
-        }
+    //    [Given(@"second number (.*)"), Scope(Tag = "Empty-Input")]
+    //    public void GivenSecondNumber(string secondNumber)
+    //    {
+    //        _ngDriver.FindElement(By.Id("Num2")).SendKeys(secondNumber);
+    //    }
 
-        [Given(@"second number (.*)"), Scope(Tag = "Negative-Input")]
-        public void GivenNumber2(string secondNumber)
-        {
-            _ngDriver.FindElement(By.Id("Num2")).SendKeys(secondNumber);
-        }
+    //    [Given(@"second number (.*)"), Scope(Tag = "Negative-Input")]
+    //    public void GivenNumber2(string secondNumber)
+    //    {
+    //        _ngDriver.FindElement(By.Id("Num2")).SendKeys(secondNumber);
+    //    }
 
 
-        [When(@"clicked Add")]
-        public void WhenClickedAdd()
-        {
-            _ngDriver.FindElement(By.Id("bAdd")).Click();
-        }
+    //    [When(@"clicked Add")]
+    //    public void WhenClickedAdd()
+    //    {
+    //        _ngDriver.FindElement(By.Id("bAdd")).Click();
+    //    }
 
-        [Then(@"Output should be (.*)")]
-        public void ThenResult(string result)
-        {
-            _ngDriver.FindElement(By.Id("lResult")).Text.ShouldBe(result);
-        }
+    //    [Then(@"Output should be (.*)")]
+    //    public void ThenResult(string result)
+    //    {
+    //        _ngDriver.FindElement(By.Id("lResult")).Text.ShouldBe(result);
+    //    }
 
-        public void Dispose()
-        {
-            _ngDriver.Dispose();
-            _driver.Dispose();
-        }
-    }
+    //    public void Dispose()
+    //    {
+    //        _ngDriver.Dispose();
+    //        _driver.Dispose();
+    //    }
+    //}
 }
